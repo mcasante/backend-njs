56256924871949ee4082c8dbe8b5a62a54dc43ce
Matheus Casante <matheus.casante@gmail.com>
2024-08-18 19:48:18 -0300

    feat: prefix the routes with api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
   await app.listen(4000);
 }
 bootstrap();
