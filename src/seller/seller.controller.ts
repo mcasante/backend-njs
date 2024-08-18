cdefef7aed37f2dc89121fc142da6d88efb75909
Matheus Casante <matheus.casante@gmail.com>
2024-08-18 17:54:49 -0300

    feat: seller list with total amount sold

--- src/seller/seller.controller.ts
+++ src/seller/seller.controller.ts
@@ -1,17 +1,13 @@
-import { Controller, Res, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SellerService } from './seller.service';
-import type { Response } from 'express';
-
-import * as sellers from '../data/sellers.json';
 
 @Controller('sellers')
 export class SellerController {
   constructor(private readonly sellerService: SellerService) {}
 
   @Get()
-  findAll(@Res() res: Response) {
-    // return this.sellerService.findAll();
-    return res.json(sellers);
+  findAll() {
+    return this.sellerService.findAll();
   }
 
   @Get(':id')
--- src/seller/seller.service.ts
+++ src/seller/seller.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Seller } from './entities/seller.entity';
 
 @Injectable()
 export class SellerService {
+  constructor(
+    @InjectRepository(Seller)
+    private sellerRepository: Repository<Seller>,
+  ) {}
+
   findAll() {
-    return `This action returns all seller`;
+    return this.sellerRepository
+      .createQueryBuilder('seller')
+      .leftJoinAndSelect('seller.orders', 'order')
+      .select('seller.id', 'id')
+      .addSelect('seller.name', 'name')
+      .addSelect('COALESCE(SUM(order.price), 0)', 'totalSales')
+      .groupBy('seller.id')
+      .getRawMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} seller`;
+    return this.sellerRepository.findOneBy({ id });
   }
 }
