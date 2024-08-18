3d92a2a713c0bd6fd9fe4fc6490942ee388fbfbc
Matheus Casante <matheus.casante@gmail.com>
2024-08-18 17:52:55 -0300

    feat: order list dto and validation

---
+++ src/order/dto/list-orders-dto.ts
@@ -0,0 +1,31 @@
+import { IsInt, IsOptional, IsIn } from 'class-validator';
+import { Type } from 'class-transformer';
+
+const sortOrder = ['ASC', 'DESC'] as const;
+const sortFields = [
+  'orderId',
+  'product',
+  'seller',
+  'country',
+  'price',
+] as const;
+
+export class ListOrdersDTO {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  limit: number = 5;
+
+  @IsOptional()
+  @IsIn(sortFields)
+  sort: (typeof sortFields)[number] = 'orderId';
+
+  @IsOptional()
+  @IsIn(sortOrder)
+  order: (typeof sortOrder)[number] = 'ASC';
+}
