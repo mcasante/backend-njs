1b95384c76c8c709a20a68ebf25fa5fcfe685d6d
Matheus Casante <matheus.casante@gmail.com>
2024-08-20 22:44:05 -0300

    refactor: rename list orders dto

--- src/order/dto/index.ts
+++ src/order/dto/index.ts
@@ -1,1 +1,1 @@
-export * from './list-orders-dto';
+export * from './list-orders.dto';
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from './entities/order.entity';
-import { ListOrdersDTO } from './dto/list-orders-dto';
+import { ListOrdersDTO } from './dto';
 
 @Injectable()
 export class OrderService {
